3f6b20583721974f48724c58c227937c7f2b241b
FraicheSolfege <littlenathan68@yahoo.com>
2024-05-14 20:01:19 -0500

    forms, models, templates, views

--- app/views.py
+++ app/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import *
+from .forms import *
+from django.shortcuts import redirect
 
 # Create your views here.
+def home(request):
+    context = {}
+    return render(request, 'home.html', context)
+
+def registerPage(request):
+    form = RegisterForm()
+    if request.method == 'POST':
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')
+    context = {'form': form}
+    return render(request, 'register.html', context)
+
+def loginPage(request):
+    return render(request, 'login.html')
--- config/urls.py
+++ config/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
+from app.views import *
 urlpatterns = [
+    path("", home, "home.html"),
+    path("login/", loginPage, "login.html"),
+    path("register/", registerPage, "register.html"),
     path('admin/', admin.site.urls),
 ]
