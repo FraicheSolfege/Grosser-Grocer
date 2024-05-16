4b7cf0e0fb8baf56ff8e1047522505ca4500719e
FraicheSolfege <littlenathan68@yahoo.com>
2024-05-15 22:58:18 -0500

    GIVE ME BACK MY INTERNET

--- app/forms.py
+++ app/forms.py
@@ -18,8 +18,3 @@ class LoginForm(forms.Form):
     username = forms.CharField(label='Username', max_length=100)
     password = forms.CharField(label='Password', max_length=100, widget=forms.PasswordInput)
 
-
-def updateTeacher(new_name:str,old_name:str):
-    teacher = Teacher.objects.get(name=old_name)
-    teacher.name = new_name
-    teacher.save()
